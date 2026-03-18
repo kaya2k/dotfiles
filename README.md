f78341f458ea68fc89fd09ff8f92b3bfc6dfd074
Seungho Lee <kayalee2k@gmail.com>
2026-03-18 18:13:06 +0900

    use kitty instead of macOS terminal

--- README.md
+++ README.md
@@ -5,10 +5,8 @@ A collection of personal config files.
 ```bash
 #!/bin/sh
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install --cask font-roboto-mono-nerd-font
+brew install --cask font-roboto-mono-nerd-font kitty
 brew install eza fish fzf git lazygit neovim ripgrep tmux tree-sitter-cli yazi zoxide
 git clone git@github.com:kaya2k/dotfiles.git ~/.dotfiles
 cd ~/.dotfiles && ./install.sh
-# Import `~/.dotfiles/profile.terminal` at macOS Terminal.
 ```
-
--- install.sh
+++ install.sh
@@ -13,11 +13,12 @@ setup() {
 }
 
 touch ~/.hushlogin
+setup "eza"     ~/.config/eza                              eza
 setup "fish"    ~/.config/fish/config.fish                 fish/config.fish
 setup "prompt"  ~/.config/fish/functions/fish_prompt.fish  fish/fish_prompt.fish
 setup "git"     ~/.gitconfig                               git/gitconfig
+setup "kitty"   ~/.config/kitty                            kitty
 setup "lazygit" ~/.config/lazygit                          lazygit
 setup "nvim"    ~/.config/nvim                             nvim
 setup "tmux"    ~/.config/tmux/tmux.conf                   tmux/tmux.conf
 setup "yazi"    ~/.config/yazi                             yazi
-setup "eza"     ~/.config/eza                              eza
